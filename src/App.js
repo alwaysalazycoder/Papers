f3e42253e9d040c4d44bb78f690070f1ea08f1f6
alwaysalazycoder <gs314315316@gmail.com>
2023-11-04 18:28:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { FcSettings, FcIdea } from "react-icons/fc";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="text-green-300 bg-black items-center py-6 px-4 text-[32px] flex">
+          <div className="w-[75%]">Papers</div>
+          <div className="flex justify-between w-[25%]  ">
+            <div>
+              <FcSettings width={10} height={10} className="w-8 h-8 spinner" />
+            </div> 
+            <div>
+              <FcIdea className=" animate-ping absolute"/>
+              <FcIdea className="relative "/>
+            </div>
+          </div>
+        </div>
+       <div className="h-[100vh] w-full bg-[#36454F]"></div>
       </header>
     </div>
   );
